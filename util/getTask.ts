c5b8028bd78987bf86b228bd89c220978d559f9b
Reuben Willson <rwillson@axiomholographics.com>
2024-12-04 10:11:57 +1000

    init

---
+++ util/getTask.ts
@@ -0,0 +1,44 @@
+import { ensureDir } from "@std/fs/ensure-dir";
+
+function requiredEnv(name: string) {
+  const ret = Deno.env.get(name);
+  if (ret === undefined) {
+    throw new Error(`Missing required environment variable "${name}"`);
+  }
+  return ret;
+}
+
+export async function getTask(day: string, stage: string) {
+  const cookieValue = requiredEnv("SESSION");
+
+  const inputFile = `./inputs/day_${day}.txt`;
+  const dayURL = `https://adventofcode.com/2024/day/${encodeURIComponent(day)}`;
+
+  const headers = new Headers();
+  headers.set("Cookie", `session=${cookieValue}`);
+
+  const input = await Deno.readTextFile(inputFile).catch(async () => {
+    console.log("downloading input");
+    await ensureDir("./inputs");
+    const ret = await (await fetch(`${dayURL}/input`, { headers })).text();
+    await Deno.writeTextFile(inputFile, ret);
+    return ret;
+  });
+
+  headers.set(
+    "content-type",
+    "application/x-www-form-urlencoded",
+  );
+
+  return {
+    input,
+    output: async (answer: string) => {
+      console.log("Answer:", answer);
+      await fetch(`${dayURL}/answer`, {
+        method: "POST",
+        body: `level=${stage}&answer=${answer}`,
+        headers,
+      });
+    },
+  };
+}
