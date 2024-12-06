b26c2b062f8b622717152a9b2124c3828206819b
Reuben Willson <rwillson@axiomholographics.com>
2024-12-06 16:57:33 +1000

    day 6 stages 1 and 2

---
+++ day_6/stage_1.ts
@@ -0,0 +1,74 @@
+import { getTask, requiredEnv } from "util/getTask.ts";
+
+export enum Direction {
+  up,
+  right,
+  down,
+  left,
+}
+
+if (import.meta.main) {
+  const task = await getTask(requiredEnv("DAY"), requiredEnv("STAGE"));
+
+  let ret = 0;
+  const map = task.input.split(/\r?\n/);
+
+  const pos = findGuard(map);
+  let dir = Direction.up;
+  const visited = new Map<number, Set<number>>([[pos.y, new Set([pos.x])]]);
+
+  while (true) {
+    const next = nextPos(pos, dir);
+    if (outsideMap(next, map)) {
+      break;
+    }
+    if (map[next.y][next.x] === "#") {
+      dir = (dir + 1) % 4 as Direction;
+      continue;
+    }
+
+    pos.x = next.x;
+    pos.y = next.y;
+    const row = visited.get(pos.y) ?? new Set();
+    row.add(pos.x);
+    visited.set(pos.y, row);
+  }
+
+  for (const row of visited.values()) {
+    ret += row.size;
+  }
+
+  await task.output(String(ret));
+}
+
+export function findGuard(map: string[]) {
+  for (let y = 0; y < map.length; ++y) {
+    const x = map[y].indexOf("^");
+    if (x !== -1) {
+      return { x, y };
+    }
+  }
+  throw new Error("No guard");
+}
+
+export function nextPos({ x, y }: { x: number; y: number }, dir: Direction) {
+  switch (dir) {
+    case Direction.up:
+      --y;
+      break;
+    case Direction.right:
+      ++x;
+      break;
+    case Direction.down:
+      ++y;
+      break;
+    case Direction.left:
+      --x;
+      break;
+  }
+  return { x, y };
+}
+
+export function outsideMap(next: { x: number; y: number }, map: string[]) {
+  return next.x < 0 || next.y < 0 || next.x >= map[0].length || next.y >= map.length;
+}
---
+++ day_6/stage_2.ts
@@ -0,0 +1,67 @@
+import { getTask, requiredEnv } from "util/getTask.ts";
+import { Direction, findGuard, nextPos, outsideMap } from "day_6/stage_1.ts";
+
+if (import.meta.main) {
+  const task = await getTask(requiredEnv("DAY"), requiredEnv("STAGE"));
+
+  let ret = 0;
+  const map = task.input.split(/\r?\n/);
+
+  const pos = findGuard(map);
+  let dir = Direction.up;
+  const visited = new Map<number, Set<number>>([[pos.y, new Set([pos.x])]]);
+
+  while (true) {
+    const next = nextPos(pos, dir);
+    if (outsideMap(next, map)) {
+      break;
+    }
+    if (map[next.y][next.x] === "#") {
+      dir = (dir + 1) % 4 as Direction;
+      continue;
+    } else if (
+      (visited.get(next.y)?.has(next.x) !== true) // We can't put an obstacle there because the guard needs to have walked through it
+    ) {
+      // What if there was an obstacle there though?
+      if (isLoop({ ...pos }, dir, map)) {
+        ++ret;
+      }
+    }
+
+    pos.x = next.x;
+    pos.y = next.y;
+    const row = visited.get(next.y) ?? new Set();
+    row.add(next.x);
+    visited.set(next.y, row);
+  }
+
+  await task.output(String(ret));
+}
+
+function isLoop(pos: { x: number; y: number }, dir: Direction, map: string[]) {
+  const visited = new Map<number, Map<number, Set<Direction>>>([[pos.y, new Map([[pos.x, new Set([dir, (dir + 1) % 4])]])]]);
+  const obstacle = nextPos(pos, dir);
+  dir = (dir + 1) % 4 as Direction;
+
+  while (true) {
+    const next = nextPos(pos, dir);
+    if (outsideMap(next, map)) {
+      return false;
+    }
+    if (map[next.y][next.x] === "#" || next.x === obstacle.x && next.y === obstacle.y) {
+      dir = (dir + 1) % 4 as Direction;
+      continue;
+    }
+
+    pos.x = next.x;
+    pos.y = next.y;
+    const row = visited.get(pos.y) ?? new Map<number, Set<Direction>>();
+    const cell = row.get(pos.x) ?? new Set<Direction>();
+    if (cell.has(dir)) {
+      return true;
+    }
+    cell.add(dir);
+    row.set(pos.x, cell);
+    visited.set(pos.y, row);
+  }
+}
