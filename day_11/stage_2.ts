f6101e2e7235a455aca3035eb6059e3cefdd0c8f
Reuben Willson <rwillson@axiomholographics.com>
2024-12-12 14:05:00 +1000

    day 11 small improvements

--- day_11/stage_2.ts
+++ day_11/stage_2.ts
@@ -1,25 +1,23 @@
 import { getTask, requiredEnv } from "util/getTask.ts";
 import { cache } from "util/cache.ts";
-import { integerDigits } from "../util/integerDigits.ts";
+import { integerDigits } from "util/integerDigits.ts";
 
-const blink = (i: number, stone: number): number => {
+const blink = cache((i: number, stone: number): number => {
   if (i === 0) {
     return 1;
   }
   if (stone === 0) {
-    return blinker(i - 1, 1);
+    return blink(i - 1, 1);
   } else {
     const digits = integerDigits(stone);
     if (digits % 2 === 0) {
       const pow10 = 10 ** (digits / 2);
-      return blinker(i - 1, Math.floor(stone / pow10)) + blinker(i - 1, stone % pow10);
+      return blink(i - 1, Math.floor(stone / pow10)) + blink(i - 1, stone % pow10);
     } else {
-      return blinker(i - 1, stone * 2024);
+      return blink(i - 1, stone * 2024);
     }
   }
-};
-
-const blinker = cache(blink);
+});
 
 if (import.meta.main) {
   const task = await getTask(requiredEnv("DAY"), requiredEnv("STAGE"));
@@ -31,7 +29,7 @@ if (import.meta.main) {
   const stones = numbers[0];
 
   for (const stone of stones) {
-    ret += blinker(75, stone);
+    ret += blink(75, stone);
   }
 
   await task.output(String(ret));
