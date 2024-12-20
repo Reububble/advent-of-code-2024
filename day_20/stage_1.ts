46bd9b94f54652970bbea75cc820dfa49885f4c4
Reuben Willson <reububble@gmail.com>
2024-12-20 22:18:26 +1000

    100ms

--- day_20/stage_1.ts
+++ day_20/stage_1.ts
@@ -1,6 +1,6 @@
 import { getTask, requiredEnv } from "util/getTask.ts";
 import { Grid } from "util/eachGrid.ts";
-import { search, searchMany } from "util/search.ts";
+import { search } from "util/search.ts";
 import { Dir2, moved, Vec2 } from "util/positions.ts";
 import { Tree } from "util/tree.ts";
 
@@ -35,36 +35,21 @@ export function benefitialCheats(benefit: number, cheat: number, chars: Grid<str
     )!.path,
   ].reverse();
 
-  const distance = Grid.create(new Array(chars.length).fill(undefined).map(() => new Array(chars[0]?.length ?? 0).fill(-1)));
-
-  normalPath.forEach((pos, i) => distance.setPos(pos, i));
-
   let ret = 0;
-  for (let i = benefit + 2; i < normalPath.length; ++i) {
+  for (let i = 0, j0 = benefit + 2; i < normalPath.length - benefit - 2; ++i, ++j0) {
     const here = normalPath[i];
-    // search outwards 20 steps for distances that are 100 less than i
-    const many = searchMany(
-      [here],
-      { depth: 2, converter: ({ x, y }) => [x, y] },
-      (v) => {
-        const offset = Math.abs(v.x - here.x) + Math.abs(v.y - here.y);
-        const pathLength = distance.atPos(v);
-        return pathLength === undefined
-          ? "Wall"
-          : offset > cheat
-          ? "Wall"
-          : chars.atPos(v) === "#"
-          ? "Walk"
-          : pathLength + offset <= i - benefit
-          ? "Win"
-          : "Walk";
-      },
-      () => 0,
-      (v) => [moved(v, "^"), moved(v, "<"), moved(v, "v"), moved(v, ">")],
-    );
-
-    ret += many.length;
+    for (let j = j0; j < normalPath.length; ++j) {
+      const dx = Math.abs(normalPath[j].x - here.x);
+      if (dx > cheat) {
+        continue;
+      }
+      const d = dx + Math.abs(normalPath[j].y - here.y);
+      if (d <= cheat && j - i - d >= benefit) {
+        ++ret;
+      }
+    }
   }
+
   return ret;
 }
 
